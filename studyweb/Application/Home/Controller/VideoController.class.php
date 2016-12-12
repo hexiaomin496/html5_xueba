6fe4871b1cc6b9a26542f60dca833ff9f3241bb2
yangxiaodi <164775925@qq.com>
2016-12-12 18:26:29 +0800

    杨晓笛添加视频详情视图和控制器

--- studyweb/Application/Home/Controller/VideoController.class.php
+++ studyweb/Application/Home/Controller/VideoController.class.php
@@ -19,13 +19,44 @@ class VideoController extends Controller{
         $show = $Page->show();
         $data = $videoModel->order('video_publish desc')->limit($Page->firstRow.','.$Page->listRows)->select();
 		
-	$this->assign('list',$data);
-	$this->assign('page',$show);
+	    $this->assign('list',$data);
+	    $this->assign('page',$show);
 
-        //热点推送
-        $hotVideo = $videoModel->order('video_count desc')->limit(8)->select();
+        //热点推送--点击量
+        $hotVideo = M('videos')->order('video_count desc')->limit(8)->select();
         $this->assign('hot',$hotVideo);
 
 	$this->display();
+    }
+
+    public function detail()
+    {
+        $condition['video_id'] = I('id');
+        $videoModel = M('videos');
+        $video = $videoModel->where($condition)->select();
+        $this->assign('video',$video);
+
+        //点击量
+        $count = M('videos')->where($condition)->setInc('video_count',1);
+
+        //上一个
+        $front = M('videos')->where('video_id<'.I('id'))->order('video_id desc')->limit('1')->find();
+        if(!$front){
+            $front['video_title'] = '';
+        }
+        $this->assign('front',$front);
+
+        //下一个
+        $next = M('videos')->where('video_id>'.I('id'))->order('video_id desc')->limit('1')->find();
+        if(!$next){
+            $next['title'] = '';
+        }
+        $this->assign('next',$next);
+
+        //热点推送--点击量
+        $hotVideo = M('videos')->order('video_count desc')->limit(8)->select();
+        $this->assign('hot',$hotVideo);
+
+        $this->display();
     } 
 }
